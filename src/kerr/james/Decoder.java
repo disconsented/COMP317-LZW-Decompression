5a6af679a0d9591d276f894621b17cd953876baf
James Kerr <jpm.kerr@outlook.com>
2018-04-28 12:20:03 +1200

    Commenting code, tweaking small things and adjusting spelling Prepping for writing the unpacker

--- src/kerr/james/Decoder.java
+++ src/kerr/james/Decoder.java
@@ -6,17 +6,16 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Stream;
 
-public class Main {
-    public static final String RESET = "\0";
+public class Decoder {
+    private static final String RESET = "\0";
     //Use the index as our lookup value
-    public static List<String> dictionary  = new ArrayList<String>();
-    public static final String test = "TOBEORNOTTOBEORTOBEEORNOT";
+    private static List<String> dictionary  = new ArrayList<>();
 
     public static void main(String[] args) {
+        //Load in our dict
         try  {
             Stream<String> stream = Files.lines(Paths.get(args[0]));
             stream.forEach(dictionary::add);
@@ -26,32 +25,34 @@ public class Main {
             return;
         }
         //So we know where to reset back to
-        int initalDictSize = dictionary.size();
-        String output = "";
+        int initialDictSize = dictionary.size();
 
         try  {
             try(BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+                //Keep reference of the last phrase for building a new one
                 String last = "";
                 for(String line; (line = br.readLine()) != null; ) {
+                    //When we get the reset symbol, remove all new entries
                     if(line.equals(RESET)){
-                        dictionary = dictionary.subList(0, initalDictSize);
+                        dictionary = dictionary.subList(0, initialDictSize);
                     } else {
                         try {
+                            //New line == new phrase
                             int phraseNumber = Integer.parseInt(line);
                             String phrase = dictionary.get(phraseNumber);
+
+                            //Just in case catch should never be true
                             if(phrase == null){
                                 System.out.println("Unknown " + phraseNumber);
                             }
 
+                            //Adding new phrases if they don't exist
                             String possibleNew = last+phrase;
-
                             if(!dictionary.contains(possibleNew)){
                                 dictionary.add(possibleNew);
-//                                System.out.println(possibleNew);
                             }
-
-                            output += phrase;
                             last = phrase;
+                            //Printing the phrases as we get them, not sure why its on a new line
                             System.out.println(last);
 
                         } catch (NumberFormatException e){
@@ -67,10 +68,6 @@ public class Main {
             e.printStackTrace();
         }
 
-        System.out.println(Arrays.toString(output.toCharArray()));
-//        System.out.println(Arrays.toString(test.toCharArray()));
-//        System.out.println(output.equals(test));
-//        System.out.println("done");
     }
 }
 
